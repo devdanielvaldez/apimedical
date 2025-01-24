80f58ccab3906ff7585fa5780b764d456b6a6822
Daniel Valdez | FullStack Developer <hello@daniel-valdez.com>
2025-01-24 18:47:27 -0400

    add connection string

--- cronJobs/registerAppointments.js
+++ cronJobs/registerAppointments.js
@@ -12,7 +12,7 @@ const registerAppointmentsInQueueJob = () => {
             const todayStart = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0, 0)); // Inicio del día UTC
             const todayEnd = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 23, 59, 59, 999)); // Fin del día UTC
 
-            console.log(`📅 Rango UTC del día: ${todayStart.toISOString()} - ${todayEnd.toISOString()}`);
+            // console.log(`📅 Rango UTC del día: ${todayStart.toISOString()} - ${todayEnd.toISOString()}`);
 
             // Buscar citas pendientes dentro del rango UTC
             const appointments = await Appointments.find({
@@ -23,20 +23,20 @@ const registerAppointmentsInQueueJob = () => {
                 statusAppointment: "PE",
             });
 
-            console.log(`\n\n📋 Citas encontradas: ${appointments.length}`);
+            // console.log(`\n\n📋 Citas encontradas: ${appointments.length}`);
 
             // Obtener IDs de las citas ya registradas en la tabla temporal
             const registeredAppointments = await TemporaryQueue.find().select("appointmentId");
             const registeredIds = registeredAppointments.map((item) => item.appointmentId.toString());
 
-            console.log(`📋 Citas ya registradas: ${registeredIds.length}`);
+            // console.log(`📋 Citas ya registradas: ${registeredIds.length}`);
 
             // Filtrar las nuevas citas que aún no están en la tabla temporal
             const newAppointments = appointments.filter(
                 (appointment) => !registeredIds.includes(appointment._id.toString())
             );
 
-            console.log(`🆕 Nuevas citas para registrar: ${newAppointments.length}`);
+            // console.log(`🆕 Nuevas citas para registrar: ${newAppointments.length}`);
 
             // Registrar las nuevas citas en la tabla temporal
             const queuePromises = newAppointments.map((appointment) =>
@@ -45,9 +45,9 @@ const registerAppointmentsInQueueJob = () => {
             await Promise.all(queuePromises);
 
             if (newAppointments.length > 0) {
-                console.log(`✅ Se registraron ${newAppointments.length} nuevas citas en la cola temporal.\n\n`);
+                // console.log(`✅ Se registraron ${newAppointments.length} nuevas citas en la cola temporal.\n\n`);
             } else {
-                console.log("📋 No hay nuevas citas para registrar.\n\n");
+                // console.log("📋 No hay nuevas citas para registrar.\n\n");
             }
         } catch (error) {
             console.error("❌ Error ejecutando el job:", error.message);
