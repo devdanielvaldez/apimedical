e041f39f44de0fc221a0892efbc960b9d38dc17b
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-01-28 13:18:59 -0400

    Update services.js

--- router/services.js
+++ router/services.js
@@ -14,10 +14,8 @@ const addServices = async (req, res) => {
     }
 
     if (!servicePrice) {
-        return res.status(400).json({
-            ok: false,
-            msg: "El costo del servicio es requerido.",
-          });
+        servicePrice = 0;
+        serviceWithInsurance = 0;
     }
 
     const text = `Servicio: ${serviceName}, Precio: ${servicePrice}, Precio con Seguro: ${serviceWithInsurance}`;
