220db5fc4848df0b0a1b58234ef10d507f5f49c5
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-01-29 14:26:01 -0400

    Update blockDates.js

--- models/blockDates.js
+++ models/blockDates.js
@@ -28,12 +28,12 @@ const BlockDatesSchema = new mongoose.Schema({
     branchOfficeId: { // Sucursal a la que pertenece
       type: mongoose.Schema.Types.ObjectId,
       ref: "BranchOffices",
-      required: true,
+      required: false,
     },
     userCreator: { // Usuario que crea el registro
       type: mongoose.Schema.Types.ObjectId,
       ref: "Users",
-      required: true,
+      required: false,
     },
     userUpdates: { // Usuario que modifica el registro
       type: mongoose.Schema.Types.ObjectId,
