b8671d3bfb8c16137d795518b95c0b39099471fc
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-01-29 14:26:45 -0400

    Update patient.js

--- models/patient.js
+++ models/patient.js
@@ -58,7 +58,7 @@ const PatientSchema = new mongoose.Schema({
   userCreator: { // Usuario que crea el registro
     type: mongoose.Schema.Types.ObjectId,
     ref: "Users",
-    required: true,
+    required: false,
   },
   userUpdates: { // Usuario que modifica el registro
     type: mongoose.Schema.Types.ObjectId,
