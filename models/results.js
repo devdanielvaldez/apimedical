f6fa7d0b7cba7adfcc169410f9b81f1127f972c0
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-01-29 14:27:02 -0400

    Update results.js

--- models/results.js
+++ models/results.js
@@ -43,7 +43,7 @@ const ResultSchema = new mongoose.Schema(
     userCreator: { // Usuario que crea el registro
       type: mongoose.Schema.Types.ObjectId,
       ref: "Users",
-      required: true,
+      required: false,
     },
     userUpdates: { // Usuario que modifica el registro
       type: mongoose.Schema.Types.ObjectId,
