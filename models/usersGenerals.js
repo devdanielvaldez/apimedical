30419be72b346f90af4a2eeb70507f51bd5af314
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-01-29 14:27:58 -0400

    Update usersGenerals.js

--- models/usersGenerals.js
+++ models/usersGenerals.js
@@ -25,7 +25,7 @@ const UsersGeneralsSchema = new mongoose.Schema({
     userCreator: { // Usuario que crea el registro
       type: mongoose.Schema.Types.ObjectId,
       ref: "Users",
-      required: true,
+      required: false,
     },
     userUpdates: { // Usuario que modifica el registro
       type: mongoose.Schema.Types.ObjectId,
