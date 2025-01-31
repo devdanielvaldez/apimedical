8164aca29274e1e26a7bdd87bee4822d6332d24a
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-01-31 07:46:09 -0400

    Update permission.js

--- models/permission.js
+++ models/permission.js
@@ -20,7 +20,7 @@ const permissionSchema = new mongoose.Schema({
     branchOfficeId: { // Sucursal a la que pertenece
       type: mongoose.Schema.Types.ObjectId,
       ref: "BranchOffices",
-      required: true,
+      required: false,
     },
     userCreator: { // Usuario que crea el registro
       type: mongoose.Schema.Types.ObjectId,
